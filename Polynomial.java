099fdbb9ca9206e9370f242f84fc3e41425baa85
lily zhou <lilyxmzhou@gmail.com>
2023-06-01 10:23:18 -0400

    s final w/ comments

--- Polynomial.java
+++ Polynomial.java
@@ -1,19 +1,23 @@
 // git checkout go to that branch
 // step 1 git add . -- everything in the folder 
 
-// step 2 git commit w/ message -, "s blah blah blah"
+// step 2 git commit w/ message -m "s blah blah blah"
 
 
 class Polynomial{
+    //global variables
     public double[] coeff;
+    // contructor no args
     Polynomial(){
         coeff = new double[1];
         coeff[0] = 0;
     }
+    //constructor w/ one double array arg
     Polynomial(double[] coeff){
         this.coeff = coeff;
     }
 
+    //add
     public Polynomial add(Polynomial p){
         double[] max_array;
         double[] min_array;
@@ -34,6 +38,8 @@ class Polynomial{
         
         
     }
+
+    //evaluate
     public double evaluate(double x){
         double sum = 0;
         for(int i = 0;i<coeff.length;i++){
@@ -41,6 +47,8 @@ class Polynomial{
         }
         return sum;
     }
+
+    //hasRoot
     public boolean hasRoot(double root){
         return this.evaluate(root) == 0;
     }
